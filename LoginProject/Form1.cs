adad693f99e2fbf8c27b77f1752f698c380be852
soyabn9 <soyabn9@users.noreply.github.com>
2019-09-13 14:32:41 +0100

    Add project files.

---
+++ LoginProject/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            // Username Label
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Username Texbox
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            // Passowrd Label
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Password Textbox
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Login Button
+        }
+    }
+}
